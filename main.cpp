4cb72238f689290816e83b10cd0a8daf90363591
tonyeko <tonyeko@users.noreply.github.com>
2020-01-16 23:27:30 +0700

    add kondisi jika n <= 0

--- main.cpp
+++ main.cpp
@@ -29,12 +29,12 @@ set<point> SearchConvexHull(vector<point> P) {
 				c = Absis(P[i])*Ordinat(P[j]) - Ordinat(P[i])*Absis(P[j]);
 				k = 0; stop = false;
 				// Inisiasi pengecekan
-				while ((EQ(P[k], P[i])) || (EQ(P[k], P[j]))) { k++; }
+				while (((EQ(P[k], P[i])) || (EQ(P[k], P[j]))) && k < P.size()) { k++; }
 				if (a*Absis(P[k])+b*Ordinat(P[k]) < c) { less = true; } 
 				else { less = false; }
-				
+				// Looping pengecekan 
 				while (k < P.size() && !stop) {
-					/* Stop saat ditemukan satu titik yang berada di sisi yang berbeda dengan titik yang lain pada garis ax+by=c*/
+					/* Stop saat ditemukan satu titik yang berada di sisi yang berbeda dengan titik yang lain pada garis ax+by=c */
 					if (not(EQ(P[k], P[i])) && not(EQ(P[k], P[j]))) {
 						if ((less && (a*Absis(P[k])+b*Ordinat(P[k]) > c)) || (!less && (a*Absis(P[k])+b*Ordinat(P[k]) < c))) {
 							stop = true;
@@ -47,12 +47,7 @@ set<point> SearchConvexHull(vector<point> P) {
 					// Jika !stop, semua titik berada di kiri atau di kanan garis (P[i] dan P[j] titik2 pembentuk convex hull)
 					result.insert(P[i]);
 					result.insert(P[j]);
-					// cout << "(" << Absis(P[i]) << "," << Ordinat(P[i]) << ")" << " dan ";
-					// cout << "(" << Absis(P[j]) << "," << Ordinat(P[j]) << ")" << endl;
-				} 
-				// else {
-				// 	cout << "GAGAL" << endl;
-				// }
+				}
 			}
 		}	
 	}
@@ -82,22 +77,27 @@ int main() {
 	/* Algoritma */
 	srand(time(NULL));
 	printf("Masukkan banyaknya titik yang diinginkan: "); cin >> n;
-	printf("\n%d Titik Acak:\n", n);
-	for (int i = 0; i < n; i++) {
-		MakeRandomPoint(&X);
-		P.push_back(X);
-		cout << "Titik ke-" << i+1 << ": " << "(" << Absis(P[i]) << "," << Ordinat(P[i]) << ")" << endl;
+	if (n > 0) {
+		printf("\n%d Titik Acak:\n", n);
+		for (int i = 0; i < n; i++) {
+			// Absis(X) = i+1; Ordinat(X) = 0;
+			MakeRandomPoint(&X);
+			P.push_back(X);
+			cout << "Titik ke-" << i+1 << ": " << "(" << Absis(P[i]) << "," << Ordinat(P[i]) << ")" << endl;
+		}
+		startTime = clock();
+		result = SearchConvexHull(P);
+		endTime = clock();
+		cout << "\nTitik-titik Pembentuk Convex Hull:" << endl;
+		for (const auto& e: result) {
+			cout << "(" << Absis(e) << "," << Ordinat(e) << ")" << endl;
+		}
+		cout << "\nWaktu untuk menemukan Convex Hull: " << ((double) (endTime - startTime)) * 1000 / CLOCKS_PER_SEC << " ms" << endl;
+		showImage(P, result);
+		return 0;
+	} else {
+		cout << "Masukan tidak sesuai!" << endl;
 	}
-	startTime = clock();
-	result = SearchConvexHull(P);
-	endTime = clock();
-	cout << "\nTitik-titik Pembentuk Convex Hull:" << endl;
-	for (const auto& e: result) {
-		cout << "(" << Absis(e) << "," << Ordinat(e) << ")" << endl;
-	}
-	cout << "\nWaktu untuk menemukan Convex Hull: " << ((double) (endTime - startTime)) * 1000 / CLOCKS_PER_SEC << " ms" << endl;
-	showImage(P, result);
-	return 0;
 }
 
 // g++ -std=c++11 main.cpp point.cpp -o main
